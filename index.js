030c75f66bbb3c0afa9c279b09f1c830b655483c
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-01-21 00:25:56 -0500

    Added step 2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const books = require('./routes/books');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use('/api/books', books);
 
 app.listen(PORT, () => {
 	console.log(`Listening on port: ${PORT}`);
