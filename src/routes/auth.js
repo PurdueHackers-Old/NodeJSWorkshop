30497ac06329c932f65124262878922e85aa2a38
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-01-22 16:39:51 -0500

    Added login route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -20,4 +20,21 @@ router.post('/signup', async (req, res) => {
 	});
 });
 
+router.post('/login', async (req, res) => {
+	const { username, password } = req.body;
+	if (!username) return res.status(400).send('Please provide a username');
+	if (!password) return res.status(400).send('Please provide a password');
+
+	let user = await User.scope('withPassword').findOne({ where: { username } });
+	if (!user) return res.status(400).send(`User with the username: ${username} does not exist`);
+
+	if (!user.comparePassword(password)) return res.status(401).send(`Incorrect password`);
+
+	const token = jwt.sign({ id: user.id }, SECRET, { expiresIn: EXPIRES_IN });
+	res.send({
+		user,
+		token
+	});
+});
+
 module.exports = router;
