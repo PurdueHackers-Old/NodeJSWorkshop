9feab091362ab8e55fcbac723fdc33053cd074fc
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-01-22 21:33:50 -0500

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 const { PORT } = require('./config');
 const { extractUser, passportMiddleware } = require('./middleware/passport');
 const books = require('./routes/books');
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(passportMiddleware(passport).initialize());
 app.use(extractUser());
 
